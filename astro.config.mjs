cd408d14a7e839a3d6b770a9b4d56df116c57dcc
Mathieu Dubart <92155925+MathieuDubart@users.noreply.github.com>
2026-05-16 15:53:36 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mathieudubart.github.io',
-  base: '/cassette-web',
+  site: 'https://getcassette.app',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
